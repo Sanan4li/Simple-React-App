04f21b278cc38a305b294e90704982e5b46c8c57
Sanan4li <Alisanan620@gmail.com>
2019-10-10 14:58:16 +0500

    Updates

---
+++ precache-manifest.420a22b52b2686284bb7325740274fcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fc4d57351a12552998d95ae8632305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac5717e33ded8d9163e",
+    "url": "/static/css/main.c42afb81.chunk.css"
+  },
+  {
+    "revision": "39459b97db9d8968c5a5",
+    "url": "/static/js/2.cabb6e14.chunk.js"
+  },
+  {
+    "revision": "0ac5717e33ded8d9163e",
+    "url": "/static/js/main.9d136602.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
